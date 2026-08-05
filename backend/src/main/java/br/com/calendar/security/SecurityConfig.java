016c9ee0b7a7a786f973b3f655d96368fe6f85f9
Azevd0 <davyson887@gmail.com>
2026-08-05 11:58:30 -0300

    criando classe de configuração de segurança

---
+++ backend/src/main/java/br/com/calendar/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package br.com.calendar.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Value("${api.password.pepper}")
+    private String pepper;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                if(rawPassword == null){
+                    return null;
+                }
+                return bcryptPasswordEncoder.encode(rawPassword + pepper);
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                if(rawPassword ==null || encodedPassword == null){
+                    return false;
+                }
+                return bcryptPasswordEncoder.matches(rawPassword + pepper, encodedPassword);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+}
