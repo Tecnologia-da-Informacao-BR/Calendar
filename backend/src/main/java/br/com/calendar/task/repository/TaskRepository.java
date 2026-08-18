54a73fbbc3af734cc6b1bcf3f4326775720215aa
João Victor Gomes Izidoro <joaovictor.gizidoro@gmail.com>
2026-08-18 19:56:12 -0300

    feat: create TaskRepository

---
+++ backend/src/main/java/br/com/calendar/task/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.calendar.task.repository;
+
+import br.com.calendar.task.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, String> {
+}
