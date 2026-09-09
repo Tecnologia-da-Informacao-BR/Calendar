036a5bed37ee8a79362b8fafd74d089abff4c3aa
Murilo de Souza Cândido <candidomurilo809@gmail.com>
2026-09-09 13:14:28 -0300

    fix: allow accented Portuguese chars in password validator
    - Change regex quantifier from * to + (require at least one valid char)
    - Add \u00C0-\u00FF range to allow accented letters (á, ã, ç, é, ê, õ, ú, etc.)
    - Emojis and characters outside allowed set remain rejected

--- frontend/src/app/shared/validators/password.ts
+++ frontend/src/app/shared/validators/password.ts
@@ -1,6 +1,6 @@
 import { Validators } from '@angular/forms';
 
-export const PASSWORD_PATTERN = /^[\x20-\x7E]*$/;
+export const PASSWORD_PATTERN = /^[\x20-\x7E\u00C0-\u00FF]+$/;
 
 export const PASSWORD_VALIDATORS = [
   Validators.required,
